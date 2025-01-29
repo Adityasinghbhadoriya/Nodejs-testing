2cc9d883f003f7dff40567ce974ed62fdb9e1c27
Adityasinghbhadoriya <aditya0303singh@gmail.com>
2025-01-29 10:37:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const PORT = 2000;
+const hostname = "localhost";
+const fs = require("fs");
+
+const home = fs.readFileSync("./index.html");
+
+console.log(__filename);
+
+const server = http.createServer((req,res) =>{
+   if(req.url === "/"){
+     return res.end(home);
+   }
+   if(req.url === "/about"){
+    return res.end("<h1>About Page</h1>");
+   }
+   
+   if(req.url === "/contact"){
+    return res.end("<h1>contact Page</h1>");
+   }
+   
+   if(req.url === "/service"){
+    return res.end("<h1>Service Page</h1>");
+   }
+   else{
+    return res.end("404 page not found")
+   }
+   
+
+//    res.end("<h1>Hello World</h1>");
+})
+
+server.listen(PORT,hostname,() =>{
+    console.log(`Server is working on http://${hostname}:${PORT}`);
+})
